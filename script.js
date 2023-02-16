8cf4260f64a689750f4def30f163fcf27715c20e
Khavushka <khavushka@gmail.com>
2023-02-16 12:51:30 +0100

    adding html tag div with id

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-import QrScanner from 'qrscanner';
+// Select elements
+const qrCodeReader = document.getElementById('qr-code-reader');
+const qrVideo = document.getElementById('qr-video');
+const qrCanvas = document.getElementById('qr-canvas');
+const qrContext = qrCanvas.getContext('2d');
 
-const video = document.getElementById('qr-video');
-
-const scanner = new QrScanner(video, result => {
-    const url = reuslt.trim();
-    window.location.href = url;
+// Set canvas size to match video size
+qrVideo.addEventListener('loadedmetadata', () => {
+  qrCanvas.width = qrVideo.videoWidth;
+  qrCanvas.height = qrVideo.videoHeight;
 });
 
-scanner.start();
+// Start video stream
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then((stream) => {
+    qrVideo.srcObject = stream;
+    qrVideo.play();
+  })
+  .catch((err) => {
+    console.error(`Error accessing camera: ${err}`);
+  });
+
+// Decode QR code
+qrVideo.addEventListener('play', () => {
+  const decode = () => {
+    qrContext.drawImage(qrVideo, 0, 0, qrCanvas.width, qrCanvas.height);
+    try {
+      const qr = new QRCodeDecoder().decode(qrCanvas);
+      console.log(`QR code detected: ${qr}`);
+    } catch (err) {
+      console.error(`Error decoding QR code: ${err}`);
+    }
+    requestAnimationFrame(decode);
+  };
+  requestAnimationFrame(decode);
+});
+
+// Stop video stream on page exit
+window.addEventListener('beforeunload', () => {
+  qrVideo.srcObject.getTracks().forEach(track => track.stop());
+});
